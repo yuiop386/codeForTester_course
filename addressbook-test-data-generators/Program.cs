659ba2f01df5eb0ac6689ffc7abdf647945af6dc
Mikhail Afanasyev <mikhail.afanasyev@emerson.com>
2022-02-16 14:44:11 +0300

    6.4 - Intermediate state

--- addressbook-test-data-generators/Program.cs
+++ addressbook-test-data-generators/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 using WebAdressbookTests;
 
 namespace addressbook_test_data_generators
@@ -12,7 +14,7 @@ namespace addressbook_test_data_generators
         static void Main(string[] args)
         {
             int count = Convert.ToInt32(args[0]);
-            StreamWriter writer = new StreamWriter(args[1]);
+            string filename = args[1];
             string format = args[2];
 
             List<GroupData> groups = new List<GroupData>();
@@ -24,19 +26,56 @@ namespace addressbook_test_data_generators
                     Footer = TestBase.GenerateRandomString(100)
                 });
             }
-            if (format == "csv")
+            if (format == "xlsx")
             {
-                writeGroupsToCsvFile(groups, writer);
-            }
-            else if (format == "xml")
-            {
-                writeGroupsToXmlFile(groups, writer);
+                writeGroupsToXlsxFile(groups, filename);
             }
             else
             {
-                System.Console.Out.Write("Unrecognized format: " + format);
+                StreamWriter writer = new StreamWriter(filename);
+                if (format == "csv")
+                {
+                    writeGroupsToCsvFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    writeGroupsToXmlFile(groups, writer);
+                }
+                else if (format == "json")
+                {
+                    writeGroupsToJsonFile(groups, writer);
+                }
+                else
+                {
+                    System.Console.Out.Write("Unrecognized format: " + format);
+                }
+                writer.Close();
             }
-            writer.Close();
+
+        }
+
+        static void writeGroupsToXlsxFile(List<GroupData> groups, string filename)
+        {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            Excel.Workbook wb = app.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+
+            int excelTableRow = 1;
+            foreach (GroupData group in groups)
+            {
+                sheet.Cells[excelTableRow, 1] = group.Name;
+                sheet.Cells[excelTableRow, 2] = group.Header;
+                sheet.Cells[excelTableRow, 3] = group.Footer;
+                excelTableRow++;
+            }
+            string fullpath = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            File.Delete(fullpath);
+            wb.SaveAs2(fullpath);
+
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
         }
 
         static void writeGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
@@ -47,11 +86,16 @@ namespace addressbook_test_data_generators
                     group.Name, group.Header, group.Footer));
             }
 
-        }
+        } 
 
         static void writeGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
         {
             new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
+        }
+
+        static void writeGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
         }
     }
 }
--- adressBook_tests/test/GroupCreationTests.cs
+++ adressBook_tests/test/GroupCreationTests.cs
@@ -6,7 +6,9 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Xml;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WebAdressbookTests
 {
@@ -52,7 +54,36 @@ namespace WebAdressbookTests
                     .Deserialize(new StreamReader(@"groups.xml"));
         }
 
-        [Test, TestCaseSource("GroupDataFromXmlFile")]
+        public static IEnumerable<GroupData> GroupDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<GroupData>>(
+                File.ReadAllText(@"groups.json"));
+        }
+
+        public static IEnumerable<GroupData> GroupDataFromXlsxFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(Path.Combine(Directory.GetCurrentDirectory(), @"groups.xlsx"));
+            Excel.Worksheet sheet = wb.ActiveSheet;
+            Excel.Range range = sheet.UsedRange;
+
+            for (int i = 1; i <= range.Rows.Count; i++)
+            {
+                groups.Add(new GroupData()
+                {
+                    Name = range.Cells[i, 1].Value,
+                    Header = range.Cells[i, 2].Value,
+                    Footer = range.Cells[i, 3].Value
+                });
+            }
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+            return groups;
+        }
+
+        [Test, TestCaseSource("GroupDataFromXlsxFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
